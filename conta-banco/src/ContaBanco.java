72bf85f33cb6708c0f98e1b9365591dafcc4c981
msribeiro2010 <msribeiro2010@icloud.com>
2023-06-25 16:36:34 -0300

    segundo commit

---
+++ conta-banco/src/ContaBanco.java
@@ -0,0 +1,13 @@
+
+public class ContaBanco {
+        private int numero;
+        private int agencia;
+        private String nomeCliente; 
+        private double saldo;
+    public ContaBanco(int numero, int agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+    }
